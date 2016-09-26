ee768d0911f3dc8424f048438a1ec31807e14265
Scott Johnson <fuzzygroup@gmail.com>
2016-09-26 13:17:21 -0400

    Rollup of final working status

--- lib/f1_history.ex
+++ lib/f1_history.ex
@@ -1,25 +1,33 @@
 defmodule F1History do
   use Application
-
-
-
-
-
-
-  # See http://elixir-lang.org/docs/stable/elixir/Application.html
-  # for more information on OTP Applications
+  
   def start(_type, _args) do
-    import Supervisor.Spec, warn: false
+      import Supervisor.Spec, warn: false
 
-    # Define workers and child supervisors to be supervised
-    children = [
-      # Starts a worker by calling: F1History.Worker.start_link(arg1, arg2, arg3)
-      # worker(F1History.Worker, [arg1, arg2, arg3]),
-    ]
+      children = [
+        supervisor(F1History.Repo, [])
+      ]
 
-    # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
-    # for other strategies and supported options
-    opts = [strategy: :one_for_one, name: F1History.Supervisor]
-    Supervisor.start_link(children, opts)
+      opts = [strategy: :one_for_one, name: F1History.Supervisor]
+      Supervisor.start_link(children, opts)
   end
+  
 end
+
+
+# # See http://elixir-lang.org/docs/stable/elixir/Application.html
+# # for more information on OTP Applications
+# def start(_type, _args) do
+#   import Supervisor.Spec, warn: false
+#
+#   # Define workers and child supervisors to be supervised
+#   children = [
+#     # Starts a worker by calling: F1History.Worker.start_link(arg1, arg2, arg3)
+#     # worker(F1History.Worker, [arg1, arg2, arg3]),
+#   ]
+#
+#   # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
+#   # for other strategies and supported options
+#   opts = [strategy: :one_for_one, name: F1History.Supervisor]
+#   Supervisor.start_link(children, opts)
+# end
